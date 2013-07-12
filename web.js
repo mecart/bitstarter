1a28b4a6040f647c1421226ecb647de6dfe58003
mecartistronico <javier.ruiz.garcia@gmail.com>
2013-07-12 03:40:20 +0000

    Trying again...

--- web.js
+++ web.js
@@ -2,14 +2,17 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var html = fs.readFileSync("index.html").toString();
+var fs = require('fs');
+
+var htmlbuf = new Buffer(fs.readFileSync('index.html'));
+var html = htmlbuf.toString();
 
 
 app.get('/', function(request, response) {
   response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+Var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
